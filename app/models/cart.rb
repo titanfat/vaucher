b0fc97e99d54654d562437de080d6bbcfe100784
titanfat <tit22an@gmail.com>
2021-02-26 09:32:04 +0300

    initial

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,12 +6,26 @@ class Cart < ApplicationRecord
   accepts_nested_attributes_for :coupon, if: lambda { |carts| carts.cart_id.blank? }
   validates_associated :coupon
 
-  def total_price(coupon = nil)
-    if coupon.present?
-      (product.price * quantity) / coupon
-    else
-      product.price * quantity
+  def total_price
+    apply_discount(price_before_percent)
+  end
+
+  def apply_discount(price, coupon = nil)
+    return price if coupon.nil?
+    if coupon.percent.present?
+      price = price - ((price / 10) * coupon.percent)
     end
+    price < 0 ? 0 : price
+  end
+
+  def discount_amount
+    price_before_percent - total_price
+  end
+
+  private
+
+  def price_before_percent
+    @_price_before_percent ||=self.inject(0) { |sum, product | sum += (product.price * product.quantity) }
   end
 
   # total_price.format

4ae9bed158ba245a389b33e0f9fc3437fd2de2ba
titanfat <tit22an@gmail.com>
2021-02-26 08:48:15 +0300

    initial

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,9 @@
 class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :admin
-  has_many :coupon , through: :user, dependent: :destroy
-  # has_one :coupon, through: :user, dependent: :destroy
-  accepts_nested_attributes_for :coupon, reject_if: lambda { |carts| carts.cart_id.blank? }
+  # has_many :coupon , through: :user, dependent: :destroy
+  has_one :coupon, through: :user, dependent: :destroy
+  accepts_nested_attributes_for :coupon, if: lambda { |carts| carts.cart_id.blank? }
   validates_associated :coupon
 
   def total_price(coupon = nil)
